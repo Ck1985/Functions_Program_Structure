a2c14f1c54fb4260a0243cdb3051fae14771f889
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2018-01-07 02:19:10 +0700

    New Commit

--- Functions_Program_Structure/Functions_Program_Structure.cpp
+++ Functions_Program_Structure/Functions_Program_Structure.cpp
@@ -1,11 +1,22 @@
 // Functions_Program_Structure.cpp : Defines the entry point for the console application.
-//
 
-#include "stdafx.h"
+#include <stdio.h>
+#define MAXLINE 1000
 
+int main() {
+	/* --------- find all lines matching pattern ---------------- */
 
-int main()
-{
-    return 0;
+	/* ---------------------------------------------------------- */
+	return;
+}
+
+/* ---------- get lines into s and return length ---------------- */
+int getline(char s[], int lim) {
+
+}
+
+/* ----------- return index of t in s, -1 if none ---------------- */
+int strIndex(char s[], char t[]) {
+
 }
 
